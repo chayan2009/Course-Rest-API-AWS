0b80f7272c535b2a7d80e1c360ad3f7eb5c5f79a
chayan <chayanchowdhury@gmail.com>
2024-10-07 23:50:33 +0530

    initial commit

---
+++ src/test/java/com/aws/springbootaws/SpringbootAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aws.springbootaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
